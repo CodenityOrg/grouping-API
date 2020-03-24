7ee6f25a7a0bbbc7707564d3605062486edca1b0
Angel <aleangrodriguez123@gmail.com>
2020-03-24 12:44:43 -0500

    config cors

--- app.js
+++ app.js
@@ -11,7 +11,22 @@ const app = express();
 
 require('./connection');
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3001',
+  'https://grouping.codenity.org'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
